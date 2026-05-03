7994aa6ba63093ff0075edccee5cfc3e3a8cec95
Sphinx <sphinxisgenuine@gmail.com>
2026-05-03 18:43:46 +0000

    Add the tokenizer take the input removes spaces and give the the input tokens

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 #include<string.h>
 #define MAX_LINE_SIZE 1024
+
+
+int tokenize (char *line,char **arg){
+int count=0;
+char *token = strtok(line, " ");
+
+while (token!=NULL) {
+    arg[count]=token;
+    count++;
+    token = strtok(NULL, " ");
+}
+arg[count]=NULL;
+return count;
+}
 int main(int argc,char *argv[]){
 char buffer[MAX_LINE_SIZE];
 
@@ -15,9 +29,12 @@ while(1){
 
      printf("You have typed %s\n",buffer);
 
+int count =tokenize(buffer,argv);
+
+for(int i  =0;i<=count;i++){
+    printf("[%d]  %s \n",i,argv[i]);
 }
-
-
+}
     return  0;
 }
 
