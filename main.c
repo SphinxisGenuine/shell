cb1af6e951b57ee5f1bef069685d15a5142f9f27
Sphinx <sphinxisgenuine@gmail.com>
2026-05-22 11:11:07 +0000

    Add fork+exe Actual Commands are working

--- main.c
+++ main.c
@@ -1,40 +1,48 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #define MAX_LINE_SIZE 1024
 
+int tokenize(char *line, char **arg) {
+  int count = 0;
+  char *token = strtok(line, " ");
 
-int tokenize (char *line,char **arg){
-int count=0;
-char *token = strtok(line, " ");
-
-while (token!=NULL) {
-    arg[count]=token;
+  while (token != NULL) {
+    arg[count] = token;
     count++;
     token = strtok(NULL, " ");
+  }
+  arg[count] = NULL;
+  return count;
 }
-arg[count]=NULL;
-return count;
-}
-int main(int argc,char *argv[]){
-char buffer[MAX_LINE_SIZE];
+int main(int argc, char *argv[]) {
+  char buffer[MAX_LINE_SIZE];
 
-while(1){
+  while (1) {
     printf("sPx>>");
     fflush(stdout);
-    if(fgets(buffer,sizeof(buffer),stdin)==NULL)
-    break;
-     buffer[strcspn(buffer, "\n")] = '\0';
+    if (fgets(buffer, sizeof(buffer), stdin) == NULL)
+      break;
+    buffer[strcspn(buffer, "\n")] = '\0';
 
-     if (strcmp(buffer,"exit")==0) break;
+    if (strcmp(buffer, "exit") == 0)
+      break;
 
-     printf("You have typed %s\n",buffer);
+    printf("You have typed %s\n", buffer);
 
-int count =tokenize(buffer,argv);
+    int count = tokenize(buffer, argv);
+    pid_t p = fork();
 
-for(int i  =0;i<=count;i++){
-    printf("[%d]  %s \n",i,argv[i]);
+    if (p == -1) {
+      printf("unable to create child process\n");
+    } else if (p == 0) {
+      execvp(argv[0], argv);
+      perror("exec failed");
+    } else {
+      waitpid(p, NULL, 0);
+    }
+  }
+  return 0;
 }
-}
-    return  0;
-}
-
