ed52c200a755057a4275b02667d1c2daa383c0d6
Sphinx <sphinxisgenuine@gmail.com>
2026-05-25 07:59:06 +0000

    Add redirection parsing and handling  <,>>,>

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,6 +6,63 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #define MAX_LINE_SIZE 1024
+typedef struct {
+  char *input_file;
+  char *output_filr;
+  int append;
+} Redirection;
+
+int parseredirection(Redirection *redir, char **argv) {
+  int i = 0;
+  int newcount = 0;
+  while (argv[i] != NULL) {
+    if (strcmp(argv[i], ">") == 0) {
+      redir->output_filr = argv[i + 1];
+      redir->append = 0;
+      i = i + 2;
+      continue;
+
+    } else if (strcmp(argv[i], ">>") == 0) {
+      redir->output_filr = argv[i + 1];
+      redir->append = 1;
+      i = i + 2;
+      continue;
+    }
+
+    else if (strcmp(argv[i], "<") == 0) {
+      redir->input_file = argv[i + 1];
+      redir->append = 0;
+      i = i + 2;
+      continue;
+    } else {
+      argv[newcount++] = argv[i++];
+    }
+  }
+  argv[newcount] = NULL;
+  return newcount;
+}
+void redirection(Redirection *redir) {
+  int flags = O_CREAT | O_WRONLY | (redir->append ? O_APPEND : O_TRUNC);
+
+  if (redir->output_filr != NULL) {
+    int fd = open(redir->output_filr, flags, 0644);
+    if (fd < 0) {
+      perror("fd:");
+      exit(1);
+    }
+    dup2(fd, 1);
+    close(fd);
+  }
+  if (redir->input_file != NULL) {
+    int fd = open(redir->input_file, O_RDONLY);
+    if (fd < 0) {
+      perror("fd:");
+      exit(1);
+    }
+    dup2(fd, 0);
+    close(fd);
+  }
+}
 
 int tokenize(char *line, char **arg) {
   int count = 0;
@@ -55,12 +113,18 @@ int main(int argc, char *argv[]) {
       printf("%s\n", buffer);
       continue;
     } else {
+      Redirection redir;
+      redir.input_file = NULL;
+      redir.output_filr = NULL;
+      redir.append = -1;
+      int count = parseredirection(&redir, cmd_argv);
       pid_t p = fork();
 
       if (p == -1) {
         printf("unable to create child process\n");
       } else if (p == 0) {
-        execvp(cmd_argv[0], argv);
+        redirection(&redir);
+        execvp(cmd_argv[0], cmd_argv);
         perror("exec failed");
       } else {
         waitpid(p, NULL, 0);
